a5e688cccd9428c6ea0a1891248cf9e3b4f8867f
Karlo Banville <karlo.banville@firstmac.com.au>
2018-03-14 14:12:22 +1000

    added parsing

--- helpers/bot.js
+++ helpers/bot.js
@@ -3,27 +3,88 @@ var mongoose = require('mongoose');
 var TimeInput = require('../models/TimeInput.js');
 
 var bot = new SlackBot({
-    token: 'TOKEN_GOES_HERE',
-    name: 'SlackBot'
+    token: 'xoxb-328763882387-tDSGGxWozp3vspLCdLez6JUt',
+    name: 'timbot'
 });
 
+var specialCommands = [
+    'list projects' // lists all project slugs
+]
 
 var onStart = () => {
     console.log('Bot started');
 }
 
-var saveTime = (userName, inputText) => {
-    TimeInput.create({
-        userName,
-        inputText
-    }, (err, post) => {
+var saveTime = (log) => {
+    TimeInput.create(log , (err, post) => {
         if (err) {
             console.log(err);
             return;
         } else {
-            bot.postMessageToUser(userName, 'Your Time was logged.', { as_user: true });
+            bot.postMessageToUser(log.userName, `Think Sumo po! Enjoy your day! \n ${log.date.toISOString().slice(0, 10)} - ${log.project}: ${log.hours}hr(s)`, { as_user: true });
         }
     });
+}
+
+var parseLine = (userName, inputText) => {
+    let items = inputText.split(' ');
+    if (items.length < 2 || items.length > 3) {
+        err = `Wat do wid dis? '${inputText}' \nShould be <date (yyyy-mm-dd) -- optional> <project-name> <# of hours>`;
+    } else {
+        // default to yesterday
+        try {
+            let date = new Date();
+            date.setDate(date.getDate() - 1);
+            // items = items.reverse();
+            console.log(items);
+            let hours = Number(items.pop());
+
+            let project = items.pop();
+            if (items.length > 0) {
+                date = new Date(items.pop());
+            }
+            let log = {
+                userName,
+                date,
+                project,
+                hours,
+                inputText
+            };
+            return log;
+        } catch (e) {
+            console.log('Error occured!', e);
+            // console.log(e);
+            err = `Wat do wid dis? '${inputText}' \nShould be <date (yyyy-mm-dd) -- optional> <project-name> <# of hours>`;
+        }
+    }
+    return err;
+}
+
+var parser = (userName, message) => {
+    let err = [];
+    let logs = [];
+    let msgArray = message.split(',');
+
+    msgArray.forEach(msg => {
+        let log = parseLine(userName, msg.trim());
+        if (typeof log === 'string')  {
+            // this means that this125 returned an error
+            err.push(log);
+        } else {
+            logs.push(log);
+        }
+    });
+
+    if (err.length > 0) {
+        err.forEach(msg => {
+            bot.postMessageToUser(userName, msg, { as_user: true });
+        });
+    } else {
+        logs.forEach((log) => {
+            saveTime(log);
+        });
+        // save time logs
+    }
 }
 
 var onMessage = (message) => {
@@ -39,8 +100,9 @@ var onMessage = (message) => {
     // var channel = channels.find(channel => channel.id === message.channel);
     var usr = users.find(user => user.id === message.user);
 
-    if(usr.name !== 'timbot' && message.user) {
-        saveTime(usr.name, message.text);
+    if(usr.name !== bot.name && message.user) {
+        // saveTime(usr.name, message.text);
+        parser(usr.name, message.text);
         // bot.postMessageToUser(usr.name, 'Your Time was logged.', {as_user: true});
     }
   }
--- models/TimeInput.js
+++ models/TimeInput.js
@@ -2,6 +2,9 @@ var mongoose = require('mongoose');
 
 var TimeInputSchema = new mongoose.Schema({
     userName: String,
+    date: Date,
+    project: String,
+    hours: Number,
     inputText: String,
     receive_date: { type: Date, default: Date.now },
 });
